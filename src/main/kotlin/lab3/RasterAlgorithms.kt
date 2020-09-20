9f02d6a5f8ac7424695456c00dcec9afad01e371
Oxana <lereenadem@yandex.ru>
2020-09-20 19:10:39 +0300

    Add lab3 template

---
+++ src/main/kotlin/lab3/RasterAlgorithms.kt
@@ -0,0 +1,60 @@
+package lab3
+
+import javafx.application.Application
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.scene.canvas.Canvas
+import javafx.scene.image.Image
+import javafx.scene.input.KeyEvent
+import javafx.scene.paint.Color
+import javafx.stage.Stage
+import lab2.SceneWrapper
+import java.io.FileInputStream
+
+class RasterAlgorithms : Application() {
+    override fun start(primaryStage: Stage) {
+        val rootScene = DemoScene(primaryStage)
+        val task1Scene = Task1(primaryStage)
+        val task2Scene = Task2(primaryStage)
+        val task3Scene = Task3(primaryStage)
+
+        primaryStage.show()
+
+        rootScene.switch()
+
+        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED) { e ->
+            when(e.text) {
+                "0" -> rootScene.switch()
+                "1" -> task1Scene.switch()
+                "2" -> task2Scene.switch()
+                "3" -> task3Scene.switch()
+            }
+        }
+    }
+
+    class DemoScene(override val primaryStage: Stage): SceneWrapper(primaryStage, "Graphics task") {
+        init {
+            val root = Group()
+            val canvas = Canvas(800.0, 600.0)
+            val gc = canvas.graphicsContext2D
+            root.children.add(canvas)
+            scene = Scene(root)
+
+            gc.stroke = Color.BLUEVIOLET
+            gc.lineWidth = 1.0
+            gc.moveTo(0.0, 0.0)
+            gc.lineTo(800.0, 600.0)
+            gc.stroke()
+
+            val imgFile = Image(FileInputStream("assets/start.png"))
+            gc.drawImage(imgFile, 0.0, 0.0, 800.0, 600.0);
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch(RasterAlgorithms::class.java, *args)
+        }
+    }
+}
---
+++ src/main/kotlin/lab3/Task3.kt
@@ -0,0 +1,7 @@
+package lab3
+
+import javafx.stage.Stage
+import lab2.SceneWrapper
+
+class Task3(override val primaryStage: Stage): SceneWrapper(primaryStage, "Task 3") {
+}
