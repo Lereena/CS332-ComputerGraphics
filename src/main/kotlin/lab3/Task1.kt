aab79e5a3432526eeb9fedb1802dbab4b23fe11b
Oxana <lereenadem@yandex.ru>
2020-09-26 20:47:45 +0300

    Add fill

--- src/main/kotlin/lab3/Task1.kt
+++ src/main/kotlin/lab3/Task1.kt
@@ -1,136 +1,93 @@
 package lab3
 
-import com.sun.glass.ui.Application
-import javafx.application.Application.launch
 import javafx.geometry.Orientation
 import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.control.ToggleButton
+import javafx.scene.image.Image
+import javafx.scene.image.WritableImage
 import javafx.scene.layout.FlowPane
+import javafx.scene.paint.Color
 import javafx.stage.Stage
 import lab2.SceneWrapper
-import java.awt.Color
-import java.util.*
-import kotlin.math.abs
+import java.io.FileInputStream
 
-var coloredPoints: Array<Array<Int>> = Array(800, { Array(600, {0}) })
-
-class Task1(override val primaryStage: Stage): SceneWrapper(primaryStage, "Task 1") {
+class Task1(override val primaryStage: Stage) : SceneWrapper(primaryStage, "Task 1") {
     init {
         val root = FlowPane(Orientation.HORIZONTAL, 0.0, 30.0)
         val canvas = Canvas(800.0, 600.0)
         val gc = canvas.graphicsContext2D
         scene = Scene(root)
         root.children.add(canvas)
+        val fillButton = ToggleButton("Активировать заливку")
+        root.children.add(fillButton)
 
         var startPoint: Point?
         var endPoint: Point? = null
-
-
-        val Button = ToggleButton("Начать заливку")
-        root.children.add(Button)
+        var image: WritableImage
+        gc.drawImage(Image(FileInputStream("assets/blank.png")), 0.0, 0.0)
 
         root.setOnMouseClicked {
             startPoint = endPoint
             endPoint = Point(it.sceneX.toInt(), it.sceneY.toInt())
-            if (startPoint != null && endPoint != null)
-                    drawLine(gc, javafx.scene.paint.Color.BLACK, startPoint!!, endPoint!!)
-        }
-
-        Button.setOnAction {
-            root.setOnMouseClicked {
-                val start = Point(it.sceneX.toInt(), it.sceneY.toInt())
-                //drawLine(gc, javafx.scene.paint.Color.RED, Point(0, 0), Point(50, 100))
-                zalivka(gc, start)
-                //for (y in (0 until 600))
-                    //for (x in (0 until 800))
-                        //if (coloredPoints[x][y] == 1)
-                            //drawLine(gc, javafx.scene.paint.Color.GREEN, Point(x,y), Point(x+1, y+1))
-                //drawLine(gc, javafx.scene.paint.Color.RED, Point(55, 105), Point(100, 150))
+            if (startPoint != null && endPoint != null) {
+                drawLine(gc, Color.BLACK, startPoint!!, endPoint!!)
             }
         }
-    }
 
-    private fun zalivka(gc: GraphicsContext, start: Point) {
-        drawLine(gc, javafx.scene.paint.Color.RED, Point(55, 105), Point(100, 150))
-        var x_l = start.x;
-        var y_l = start.y;
-        var x_r = start.x;
-        var y_r = start.y;
-        if (coloredPoints[start.x][start.y] == 0) {
-            while (coloredPoints[x_l][y_l] != 1 && x_l > 0) {
-                if (y_l + 1 < 600 && coloredPoints[x_l][y_l + 1] != 1)
-                    zalivka(gc, Point(x_l, y_l + 1))
-                //if (y_l - 1 > 0 && coloredPoints[x_l][y_l - 1] != 1)
-                  //  zalivka(gc, Point(x_l, y_l - 1))
-                x_l -= 1;
-            }
-
-            while (coloredPoints[x_r][y_r] != 1 && x_r < 800) {
-                if (y_l + 1 < 600 && coloredPoints[x_l][y_l + 1] != 1)
-                    zalivka(gc, Point(x_r, y_l + 1))
-                //if (y_l - 1 > 0 && coloredPoints[x_l][y_l - 1] != 1)
-                  //  zalivka(gc, Point(x_r, y_l - 1))
-                x_r += 1;
-            }
-            while (x_l < x_r) {
-                if (y_l + 1 < 600 && coloredPoints[x_l][y_l + 1] != 1)
-                    zalivka(gc, Point(x_l, y_l + 1))
-                if (y_l - 1 > 0 && coloredPoints[x_l][y_l - 1] != 1)
-                    zalivka(gc, Point(x_l, y_l - 1))
-                x_l += 1
-            }
-            drawLine(gc, javafx.scene.paint.Color.RED, Point(x_l, y_l), Point(x_r, y_r))
-        }
-
-        //drawLine(gc, javafx.scene.paint.Color.RED, startPoint!!, endPoint!!)
-    }
-
-    private fun drawLine(gc: GraphicsContext, color: javafx.scene.paint.Color, start: Point, end: Point) {
-        val line = getLine(javafx.scene.paint.Color.BLACK, start, end)
-        val pixelWriter = gc.pixelWriter
-        for (point in line) {
-            pixelWriter.setColor(point.x, point.y, color)
-            coloredPoints[point.x][point.y] = 1
-        }
-    }
-
-    private fun getLine(color: javafx.scene.paint.Color, start: Point, end: Point): LinkedList<Point> {
-        val result = LinkedList<Point>()
-        result.add(start)
-
-        val a = end.y - start.y
-        val b = start.x - end.x
-        val sign = if (abs(a) > abs(b)) 1 else -1
-        val signA = if (a > 0) 1 else -1
-        val signB = if (b > 0) 1 else -1
-
-        var f = 0
-        var x = start.x
-        var y = start.y
-
-        if (sign == -1)
-            do {
-                f += a * signA
-                if (f > 0) {
-                    f -= b * signB
-                    y += signA
+        fillButton.setOnAction {
+            if (fillButton.isSelected) {
+                image = canvas.snapshot(null, null)
+                root.setOnMouseClicked {
+                    val start = Point(it.sceneX.toInt(), it.sceneY.toInt())
+                    fill(gc, start, Color.GREEN, image)
                 }
-                x -= signB
-                result.add(Point(x, y, color))
-            } while (x != end.x || y != end.y)
-        else
-            do {
-                f += b * signB
-                if (f > 0) {
-                    f -= a * signA
-                    x -= signB
+            }
+            else {
+                root.setOnMouseClicked {
+                    startPoint = endPoint
+                    endPoint = Point(it.sceneX.toInt(), it.sceneY.toInt())
+                    if (startPoint != null && endPoint != null) {
+                        drawLine(gc, Color.BLACK, startPoint!!, endPoint!!)
+                    }
                 }
-                y += signA
-                result.add(Point(x, y, color))
-            } while (x != end.x || y != end.y)
+            }
+        }
+    }
 
-        return result
+    fun fill(
+        gc: GraphicsContext, startPoint: Point, targetColor: Color, image: WritableImage,
+        first: Boolean = true, areaColor: Color? = null, direction: Direction? = null
+    ) {
+        val pixelReader = image.pixelReader
+        val pixelColor = pixelReader.getColor(startPoint.x, startPoint.y)
+        if (noNeedToHandle(areaColor, pixelColor, targetColor))
+            return
+
+        var xL = startPoint.x
+        var xR = startPoint.x
+        val y = startPoint.y
+
+        while (pixelReader.getColor(xL, y) == pixelColor) xL--
+        while (pixelReader.getColor(xR, y) == pixelColor) xR++
+        val pixelWriter = image.pixelWriter
+        for (x in (xL + 1) until xR)
+            pixelWriter.setColor(x, y, targetColor)
+        gc.drawImage(image, 0.0, 0.0)
+
+        for (x in (xL + 1) until xR) {
+            if (first || direction == Direction.UP)
+                fill(gc, Point(x, y + 1), targetColor, image, false, pixelColor, Direction.UP)
+            if (first || direction == Direction.DOWN)
+                fill(gc, Point(x, y - 1), targetColor, image, false, pixelColor, Direction.DOWN)
+        }
+    }
+
+    private fun noNeedToHandle(areaColor: Color?, currentColor: Color, targetColor: Color): Boolean {
+        return (areaColor != null && currentColor != areaColor)
+                || currentColor == targetColor
     }
 }
+
+enum class Direction { UP, DOWN }
--- src/main/kotlin/lab3/Task3.kt
+++ src/main/kotlin/lab3/Task3.kt
@@ -38,130 +38,130 @@ class Task3(override val primaryStage: Stage) : SceneWrapper(primaryStage, "Task
                     drawLine(gc, Color.BLACK, startPoint!!, endPoint!!)
         }
     }
+}
 
-    private fun drawLine(gc: GraphicsContext, color: Color, start: Point, end: Point) {
-        val line = getLine(Color.BLACK, start, end)
-        val pixelWriter = gc.pixelWriter
-        for (point in line)
-            pixelWriter.setColor(point.x, point.y, color)
-    }
+fun drawLine(gc: GraphicsContext, color: Color, start: Point, end: Point) {
+    val line = getLine(Color.BLACK, start, end)
+    val pixelWriter = gc.pixelWriter
+    for (point in line)
+        pixelWriter.setColor(point.x, point.y, color)
+}
 
-    private fun getLine(color: Color, start: Point, end: Point): LinkedList<Point> {
-        val result = LinkedList<Point>()
-        result.add(start)
+private fun getLine(color: Color, start: Point, end: Point): LinkedList<Point> {
+    val result = LinkedList<Point>()
+    result.add(start)
 
-        val a = end.y - start.y
-        val b = start.x - end.x
-        val sign = if (abs(a) > abs(b)) 1 else -1
-        val signA = if (a > 0) 1 else -1
-        val signB = if (b > 0) 1 else -1
+    val a = end.y - start.y
+    val b = start.x - end.x
+    val sign = if (abs(a) > abs(b)) 1 else -1
+    val signA = if (a > 0) 1 else -1
+    val signB = if (b > 0) 1 else -1
 
-        var f = 0
-        var x = start.x
-        var y = start.y
+    var f = 0
+    var x = start.x
+    var y = start.y
 
-        if (sign == -1)
-            do {
-                f += a * signA
-                if (f > 0) {
-                    f -= b * signB
-                    y += signA
-                }
-                x -= signB
-                result.add(Point(x, y, color))
-            } while (x != end.x || y != end.y)
-        else
-            do {
-                f += b * signB
-                if (f > 0) {
-                    f -= a * signA
-                    x -= signB
-                }
+    if (sign == -1)
+        do {
+            f += a * signA
+            if (f > 0) {
+                f -= b * signB
                 y += signA
-                result.add(Point(x, y, color))
-            } while (x != end.x || y != end.y)
-
-        return result
-    }
-
-    fun drawWuLine(gc: GraphicsContext, color: Color, start: Point, end: Point) {
-        val line = getWuLine(color, start, end)
-        val pixelWriter = gc.pixelWriter
-        for (point in line)
-            pixelWriter.setColor(point.x, point.y, point.color)
-    }
-
-    fun getWuLine(color: Color, start: Point, end: Point): LinkedList<Point> {
-        val result = LinkedList<Point>()
-        val dx = abs(start.x - end.x)
-        val dy = abs(start.y - end.y)
-        if (dx == 0 || dy == 0)
-            return getLine(color, start, end)
-
-        var x0 = start.x
-        var x1 = end.x
-        var y0 = start.y
-        var y1 = end.y
-
-        if (dy < dx) {
-            if (x1 < x0) {
-                x1 += x0; x0 = x1 - x0; x1 -= x0
-                y1 += y0; y0 = y1 - y0; y1 -= y0
             }
-            val grad = if (y1 > y0) 1.0 * dy / dx else - 1.0 * dy / dx
-            var intery = y0 + grad
-            result.add(Point(x0, y0, Color(color.red, color.green, color.blue, 1.0)))
+            x -= signB
+            result.add(Point(x, y, color))
+        } while (x != end.x || y != end.y)
+    else
+        do {
+            f += b * signB
+            if (f > 0) {
+                f -= a * signA
+                x -= signB
+            }
+            y += signA
+            result.add(Point(x, y, color))
+        } while (x != end.x || y != end.y)
 
-            for (x in (x0 + 1) until x1) {
-                result.add(
-                    Point(
-                        x, intery.toInt(),
-                        Color(
-                            color.red, color.blue, color.green,
-                            (255 - (intery - intery.toInt()) * 255) / 255
-                        )
-                    ))
-                result.add(
-                    Point(
-                        x, intery.toInt() + 1,
-                        Color(
-                            color.red, color.blue, color.green,
-                            (intery - intery.toInt())
+    return result
+}
+
+fun drawWuLine(gc: GraphicsContext, color: Color, start: Point, end: Point) {
+    val line = getWuLine(color, start, end)
+    val pixelWriter = gc.pixelWriter
+    for (point in line)
+        pixelWriter.setColor(point.x, point.y, point.color)
+}
+
+private fun getWuLine(color: Color, start: Point, end: Point): LinkedList<Point> {
+    val result = LinkedList<Point>()
+    val dx = abs(start.x - end.x)
+    val dy = abs(start.y - end.y)
+    if (dx == 0 || dy == 0)
+        return getLine(color, start, end)
+
+    var x0 = start.x
+    var x1 = end.x
+    var y0 = start.y
+    var y1 = end.y
+
+    if (dy < dx) {
+        if (x1 < x0) {
+            x1 += x0; x0 = x1 - x0; x1 -= x0
+            y1 += y0; y0 = y1 - y0; y1 -= y0
+        }
+        val grad = if (y1 > y0) 1.0 * dy / dx else - 1.0 * dy / dx
+        var intery = y0 + grad
+        result.add(Point(x0, y0, Color(color.red, color.green, color.blue, 1.0)))
+
+        for (x in (x0 + 1) until x1) {
+            result.add(
+                Point(
+                    x, intery.toInt(),
+                    Color(
+                        color.red, color.blue, color.green,
+                        (255 - (intery - intery.toInt()) * 255) / 255
+                    )
+                ))
+            result.add(
+                Point(
+                    x, intery.toInt() + 1,
+                    Color(
+                        color.red, color.blue, color.green,
+                        (intery - intery.toInt())
                     )))
-                intery += grad
-            }
-            result.add(Point(x1, y1, Color(color.red, color.green, color.blue, 1.0)))
+            intery += grad
         }
-        else {
-            if (y1 < y0) {
-                x1 += x0; x0 = x1 - x0; x1 -= x0
-                y1 += y0; y0 = y1 - y0; y1 -= y0
-            }
-            val gradient = if (x1 > x0) 1.0 * dx / dy else - 1.0 * dx / dy
-            var interx = x0 + gradient
-            result.add(Point(x0, y0, Color(color.red, color.green, color.blue, 1.0)))
-
-            for (y in (y0 + 1) until y1) {
-                result.add(
-                    Point(
-                        interx.toInt(), y,
-                        Color(
-                            color.red, color.blue, color.green,
-                            (255 - (interx - interx.toInt()) * 255) / 255
-                        )
-                    ))
-                result.add(
-                    Point(
-                        interx.toInt() + 1, y,
-                        Color(
-                            color.red, color.blue, color.green,
-                            (interx - interx.toInt())
-                        )))
-                interx += gradient
-            }
-            result.add(Point(x1, y1, Color(color.red, color.green, color.blue, 1.0)))
-        }
-
-        return result
+        result.add(Point(x1, y1, Color(color.red, color.green, color.blue, 1.0)))
     }
+    else {
+        if (y1 < y0) {
+            x1 += x0; x0 = x1 - x0; x1 -= x0
+            y1 += y0; y0 = y1 - y0; y1 -= y0
+        }
+        val gradient = if (x1 > x0) 1.0 * dx / dy else - 1.0 * dx / dy
+        var interx = x0 + gradient
+        result.add(Point(x0, y0, Color(color.red, color.green, color.blue, 1.0)))
+
+        for (y in (y0 + 1) until y1) {
+            result.add(
+                Point(
+                    interx.toInt(), y,
+                    Color(
+                        color.red, color.blue, color.green,
+                        (255 - (interx - interx.toInt()) * 255) / 255
+                    )
+                ))
+            result.add(
+                Point(
+                    interx.toInt() + 1, y,
+                    Color(
+                        color.red, color.blue, color.green,
+                        (interx - interx.toInt())
+                    )))
+            interx += gradient
+        }
+        result.add(Point(x1, y1, Color(color.red, color.green, color.blue, 1.0)))
+    }
+
+    return result
 }
