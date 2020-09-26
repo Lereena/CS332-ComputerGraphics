25bd0f7b04ef134d26c09e27028f182b92e2ddb0
Oxana <lereenadem@yandex.ru>
2020-09-26 21:20:25 +0300

    Add image upload

--- src/main/kotlin/lab3/Task1.kt
+++ src/main/kotlin/lab3/Task1.kt
@@ -9,8 +9,10 @@ import javafx.scene.image.Image
 import javafx.scene.image.WritableImage
 import javafx.scene.layout.FlowPane
 import javafx.scene.paint.Color
+import javafx.stage.FileChooser
 import javafx.stage.Stage
 import lab2.SceneWrapper
+import java.io.File
 import java.io.FileInputStream
 
 class Task1(override val primaryStage: Stage) : SceneWrapper(primaryStage, "Task 1") {
@@ -20,39 +22,54 @@ class Task1(override val primaryStage: Stage) : SceneWrapper(primaryStage, "Task
         val gc = canvas.graphicsContext2D
         scene = Scene(root)
         root.children.add(canvas)
-        val fillButton = ToggleButton("Активировать заливку")
-        root.children.add(fillButton)
+
+        val lineDrawButton = ToggleButton("Рисовать линии")
+        val colorFillButton = ToggleButton("Заливать цветом")
+        val imageFillButton = ToggleButton("Заливать изображением")
+
+        val fileChooser = FileChooser()
+        fileChooser.title = "Выберите изображение"
+        val extensionFilter = FileChooser.ExtensionFilter("Image Files", "*.png")
+        fileChooser.extensionFilters.add(extensionFilter)
+        fileChooser.initialDirectory = File("assets")
+        root.children.addAll(lineDrawButton, colorFillButton, imageFillButton)
 
         var startPoint: Point?
         var endPoint: Point? = null
-        var image: WritableImage
+        var canvasImage: WritableImage
+        var fillImage: Image
         gc.drawImage(Image(FileInputStream("assets/blank.png")), 0.0, 0.0)
 
-        root.setOnMouseClicked {
-            startPoint = endPoint
-            endPoint = Point(it.sceneX.toInt(), it.sceneY.toInt())
-            if (startPoint != null && endPoint != null) {
-                drawLine(gc, Color.BLACK, startPoint!!, endPoint!!)
+        lineDrawButton.setOnAction {
+            colorFillButton.isSelected = false
+            imageFillButton.isSelected = false
+
+            root.setOnMouseClicked {
+                startPoint = endPoint
+                endPoint = Point(it.sceneX.toInt(), it.sceneY.toInt())
+                if (startPoint != null && endPoint != null) {
+                    drawLine(gc, Color.BLACK, startPoint!!, endPoint!!)
+                }
             }
         }
 
-        fillButton.setOnAction {
-            if (fillButton.isSelected) {
-                image = canvas.snapshot(null, null)
-                root.setOnMouseClicked {
-                    val start = Point(it.sceneX.toInt(), it.sceneY.toInt())
-                    fill(gc, start, Color.GREEN, image)
-                }
+        colorFillButton.setOnAction {
+            lineDrawButton.isSelected = false
+            imageFillButton.isSelected = false
+
+            canvasImage = canvas.snapshot(null, null)
+            root.setOnMouseClicked {
+                val start = Point(it.sceneX.toInt(), it.sceneY.toInt())
+                fill(gc, start, Color.GREEN, canvasImage)
             }
-            else {
-                root.setOnMouseClicked {
-                    startPoint = endPoint
-                    endPoint = Point(it.sceneX.toInt(), it.sceneY.toInt())
-                    if (startPoint != null && endPoint != null) {
-                        drawLine(gc, Color.BLACK, startPoint!!, endPoint!!)
-                    }
-                }
-            }
+        }
+
+        imageFillButton.setOnAction {
+            lineDrawButton.isSelected = false
+            imageFillButton.isSelected = false
+            val selectedFile = fileChooser.showOpenDialog(primaryStage)
+            if (selectedFile != null)
+                fillImage = Image(FileInputStream(selectedFile))
         }
     }
 
