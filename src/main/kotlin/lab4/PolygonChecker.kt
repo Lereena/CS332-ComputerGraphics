45cd5f834805b6f9514449d7989bcc955af2d4ca
elomakina <elomakina1906@yandex.ru>
2020-09-29 03:32:24 +0300

    failed attempt to finish

--- src/main/kotlin/lab4/PolygonChecker.kt
+++ src/main/kotlin/lab4/PolygonChecker.kt
@@ -12,44 +12,84 @@ fun checkIsIn(shape: Shape, startPoint: Point) : Boolean {
     var cnt = 0;
     var cur = shape.points.first
     for (next in shape.points)
-        if (next.y >= y && cur.y <= y || next.y <= y && cur.y >= y)
-            if (x < next.x || x < cur.x)
+        if (next.y >= y && cur.y <= y || next.y <= y && cur.y >= y) {
+            val x_rib = (next.x * cur.y - cur.x * next.y
+                    - (next.x - cur.x) * y) / (next.x - cur.x)
+            if (x < x_rib)
                 cnt++
+        }
     if (cnt % 2 == 0)
         return false
     return true
 }
 
+
 fun checkEdgesIntersection(edges: Vector<Shape>): Vector<Point> {
     val result = Vector<Point>()
-    // TODO: Найти пересечения между парами отрезков
-    throw NotImplementedError("NOT IMPLEMENTED")
+    for (main_line in edges) {
+        for (other_line in edges) {
+            //проверка пересекаются ли main_line и other_line,
+            // если да — точку пересечения в result
+        }
+    }
+    return result
+            //throw NotImplementedError("NOT IMPLEMENTED")
+}
+
+fun polygonIsConvex(shape: Shape) : Convexity {
+    var cur_line = shape.points.first
+    var flag0 = false
+    var flag1 = false
+    for (next_line in shape.points) {
+        if (flag0) {
+            var cur_rib = shape.points.first
+            for (next_rib in shape.points) {
+                if (flag1) {
+                    // Значения функции на точках ребер
+                    val x0 = (next_line.x * cur_line.y - cur_line.x * next_line.y
+                            - (cur_line.y - next_line.y) * cur_rib.x) / (next_line.x - cur_line.x)
+                    val x1 = (next_line.x * cur_line.y - cur_line.x * next_line.y
+                            - (cur_line.y - next_line.y) * next_rib.x) / (next_line.x - cur_line.x)
+                    if (x0 > cur_rib.y && x1 < next_rib.y || x0 < cur_rib.y && x1 > next_rib.y)
+                        return Convexity.NonConvex
+                }
+                flag1 = true
+                cur_rib = next_rib
+            }
+        }
+        flag0 = true
+        cur_line = next_line
+    }
+    return Convexity.Convex
 }
 
 fun checkPolygon(shape: Shape, startPoint: Point) : Convexity {
     if (!checkIsIn(shape, startPoint))
         return Convexity.Undefined
-    //тут вместо else определяем выпуклый ли полигон
-    else return Convexity.Convex
+    else return polygonIsConvex(shape)
 }
 
 class PolygonsCheckResult (val convex_count: Int, val nonconvex_count: Int) {}
 
 fun checkPolygons(shapes: Vector<Shape>, startPoint: Point) : PolygonsCheckResult {
-    /* TODO: Проверить, входит ли точка в какие-либо многоугольники.
-        Вернуть количество выпуклых и невыпуклых многоугольников
-        Думаю, что если фигура состоит из 1/2 точек, то можно игнорировать
-    */
-//    throw NotImplementedError("NOT IMPLEMENTED")
-
-    return PolygonsCheckResult(0, 0)
+    var cnt_convex = 0
+    var cnt_non_convex = 0
+    for (s in shapes) {
+        if (checkPolygon(s, startPoint) == Convexity.Convex)
+            cnt_convex++
+        if (checkPolygon(s, startPoint) == Convexity.NonConvex)
+            cnt_non_convex++
+    }
+    return PolygonsCheckResult(cnt_convex, cnt_non_convex)
 }
 
 fun checkPointEdge(edge: Shape, startPoint: Point) : Position {
     //если yb·xa - xb·ya > 0 => b слева от Oa
     //если yb·xa - xb·ya < 0 => b справа от Oa
-    val a = edge.points.first
-    val b = edge.points.last
+    var a = Point(edge.points.last.x - edge.points.first.x,
+                  edge.points.last.y - edge.points.first.y)
+    var b = Point(startPoint.x - edge.points.first.x,
+                     startPoint.y - edge.points.first.y)
     if (b.y * a.x - b.x * a.y > 0)
         return Position.Left
     if (b.y * a.x - b.x * a.y < 0)
