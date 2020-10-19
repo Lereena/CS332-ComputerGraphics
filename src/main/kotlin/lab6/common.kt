1ad8db38f2efca24880c57a75bc3d5536d55ab23
Oxana <lereenadem@yandex.ru>
2020-10-19 13:30:55 +0300

    Add line by two points

--- src/main/kotlin/lab6/common.kt
+++ src/main/kotlin/lab6/common.kt
@@ -6,14 +6,14 @@ data class Point3D(var x: Double, var y: Double, var z: Double)
 
 enum class Axis { X, Y, Z }
 
-data class UnitVector(val l: Double, val m: Double, val n: Double)
+data class DirectionVector(val l: Double, val m: Double, val n: Double)
 
-data class Line(var point: Point3D, var vector: UnitVector) {
+data class Line(var point: Point3D, var vector: DirectionVector) {
 
     constructor(point1: Point3D, point2: Point3D) {
         if (point1 == point1)
             throw InvalidParameterException("Прямая не может быть задана совпадающими точками")
         point = point1
-        TODO("Not yet implemented")
+        vector = DirectionVector(point2.x - point1.x, point2.y - point1.y, point2.z - point1.z)
     }
 }
