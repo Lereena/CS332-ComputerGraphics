0467eb4e50358629c2f0f4435e0b249fcf53b222
Oxana <lereenadem@yandex.ru>
2020-10-19 14:43:20 +0300

    Change objects, add matrices

--- src/main/kotlin/lab6/Transformations.kt
+++ src/main/kotlin/lab6/Transformations.kt
@@ -1,6 +1,8 @@
 package lab6
 
 import java.util.*
+import kotlin.math.cos
+import kotlin.math.sin
 
 fun move(polyhedron: LinkedList<Point3D>, dx: Int, dy: Int, dz: Int): LinkedList<Point3D> {
     TODO("Not yet implemented")
@@ -21,3 +23,56 @@ fun scale(polyhedron: LinkedList<Point3D>, kX: Double, kY: Double, kZ: Double): 
 fun reflect(polyhedron: LinkedList<Point3D>, axis1: Axis, axis2: Axis): LinkedList<Point3D> {
     TODO("Not yet implemented")
 }
+
+val identityMatrix = arrayOf(
+    doubleArrayOf(1.0, 0.0, 0.0, 0.0),
+    doubleArrayOf(0.0, 1.0, 0.0, 0.0),
+    doubleArrayOf(0.0, 0.0, 1.0, 0.0),
+    doubleArrayOf(0.0, 0.0, 0.0, 1.0),
+)
+
+fun translationMatrix(tX: Double, tY: Double, tZ: Double): Array<DoubleArray> {
+    return arrayOf(
+        doubleArrayOf(1.0, 0.0, 0.0, tX),
+        doubleArrayOf(0.0, 1.0, 0.0, tY),
+        doubleArrayOf(0.0, 0.0, 1.0, tZ),
+        doubleArrayOf(0.0, 0.0, 0.0, 1.0),
+    )
+}
+
+fun scaleMatrix(mX: Double, mY: Double, mZ: Double): Array<Array<Double>> {
+    return arrayOf(
+        arrayOf(mX, 0.0, 0.0, 0.0),
+        arrayOf(0.0, mY, 0.0, 0.0),
+        arrayOf(0.0, 0.0, mZ, 0.0),
+        arrayOf(0.0, 0.0, 0.0, 1.0),
+    )
+}
+
+fun rotationXMatrix(angle: Double): Array<DoubleArray> {
+    return arrayOf(
+        doubleArrayOf(1.0, 0.0, 0.0, 0.0),
+        doubleArrayOf(0.0, cos(angle), -sin(angle), 0.0),
+        doubleArrayOf(0.0, sin(angle), cos(angle), 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 1.0),
+    )
+}
+
+fun rotationYMatrix(angle: Double): Array<DoubleArray> {
+    return arrayOf(
+        doubleArrayOf(cos(angle), 0.0, sin(angle), 0.0),
+        doubleArrayOf(0.0, 1.0, 0.0, 0.0),
+        doubleArrayOf(-sin(angle), 0.0, cos(angle), 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 1.0),
+    )
+}
+
+fun rotationZMatrix(angle: Double): Array<DoubleArray> {
+    return arrayOf(
+        doubleArrayOf(cos(angle), -sin(angle), 0.0, 0.0),
+        doubleArrayOf(sin(angle), cos(angle), 0.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 1.0, 0.0),
+        doubleArrayOf(0.0, 0.0, 0.0, 1.0),
+    )
+}
+
--- src/main/kotlin/lab6/common.kt
+++ src/main/kotlin/lab6/common.kt
@@ -1,6 +1,6 @@
 package lab6
 
-import java.security.InvalidParameterException
+import java.util.*
 
 data class Point3D(var x: Double, var y: Double, var z: Double)
 
@@ -8,12 +8,10 @@ enum class Axis { X, Y, Z }
 
 data class DirectionVector(val l: Double, val m: Double, val n: Double)
 
-data class Line(var point: Point3D, var vector: DirectionVector) {
+typealias Polyhedron = LinkedList<Polygon>
 
-    constructor(point1: Point3D, point2: Point3D) {
-        if (point1 == point1)
-            throw InvalidParameterException("Прямая не может быть задана совпадающими точками")
-        point = point1
-        vector = DirectionVector(point2.x - point1.x, point2.y - point1.y, point2.z - point1.z)
-    }
+typealias Polygon = LinkedList<Line>
+
+data class Line(val point1: Point3D, val point2: Point3D) {
+    val directionVector = DirectionVector(point2.x - point1.x, point2.y - point1.y, point2.z - point1.z)
 }
